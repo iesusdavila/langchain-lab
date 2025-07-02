8517c75f4c9b1fd0b542d52506123fc38bdcd80f
Iesus Davila <iesusdavila@gmail.com>
2025-07-02 18:19:31 -0500

    Examples using Prompt Template, Conversation Buffer Memory, and LLMChain (smart)

---
+++ class_1/example1.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from langchain import PromptTemplate
+from langchain_community.llms.llamacpp import LlamaCpp
+
+st.title("LangChain con Streamlit y llama.cpp")
+input_text = st.text_input("Ingresa tu prompt:")
+
+prompt = PromptTemplate(
+    input_variables=["topic"],
+    template="Dime algo acerca de esta celebridad {topic}",
+)
+
+llm = LlamaCpp(
+    model_path="models/models--ggml-org--Meta-Llama-3.1-8B-Instruct-Q4_0-GGUF/snapshots/0aba27dd2f1c7f4941a94a5c59d80e0a256f9ff8/meta-llama-3.1-8b-instruct-q4_0.gguf",        
+    n_ctx=2048,
+    verbose=True,
+    n_gpu_layers=10,  
+    n_threads=8
+)
+
+if input_text:
+    chain = prompt | llm
+    result = chain.invoke({"topic": input_text})
+    
+    st.write(result)
---
+++ class_1/example2.py
@@ -0,0 +1,76 @@
+import streamlit as st
+from langchain.prompts import PromptTemplate
+from langchain.memory import ConversationBufferMemory
+from langchain.chains.sequential import SequentialChain
+from langchain_community.llms.llamacpp import LlamaCpp
+from langchain_experimental.smart_llm.base import SmartLLMChain
+
+st.title('Celebrity Search Results')
+input_text = st.text_input("Search the topic you want")
+
+# Prompt Templates
+first_input_prompt = PromptTemplate(
+    input_variables=['name'],
+    template="Tell me about celebrity {name}"
+)
+second_input_prompt = PromptTemplate(
+    input_variables=['person'],
+    template="When was {person} born?"
+)
+third_input_prompt = PromptTemplate(
+    input_variables=['dob'],
+    template="Mention 5 major events that happened around {dob} in the world."
+)
+
+person_memory = ConversationBufferMemory(input_key='name', memory_key='chat_history')
+dob_memory = ConversationBufferMemory(input_key='person', memory_key='chat_history')
+descr_memory = ConversationBufferMemory(input_key='dob', memory_key='description_history')
+
+llm = LlamaCpp(
+    model_path="models/models--ggml-org--Meta-Llama-3.1-8B-Instruct-Q4_0-GGUF/snapshots/0aba27dd2f1c7f4941a94a5c59d80e0a256f9ff8/meta-llama-3.1-8b-instruct-q4_0.gguf",        
+    n_ctx=2048,
+    temperature=0.8,
+    verbose=True,
+    n_threads=8,
+    n_gpu_layers=20
+)
+
+chain1 = SmartLLMChain(
+    llm=llm, 
+    prompt=first_input_prompt,
+    output_key='person',
+    memory=person_memory,
+    verbose=True
+)
+
+chain2 = SmartLLMChain(
+    llm=llm,
+    prompt=second_input_prompt,
+    output_key='dob',
+    memory=dob_memory,
+    verbose=True
+)
+
+chain3 = SmartLLMChain(
+    llm=llm,
+    prompt=third_input_prompt,
+    output_key='description',
+    memory=descr_memory,
+    verbose=True
+)
+
+parent_chain = SequentialChain(
+    chains=[chain1, chain2, chain3],
+    input_variables=['name'],
+    output_variables=['person', 'dob', 'description'],
+    verbose=True
+)
+
+if input_text:
+    result = parent_chain({'name': input_text})
+    st.write(result)
+
+    with st.expander('Person Name'):
+        st.info(person_memory.buffer)
+    with st.expander('Major Events'):
+        st.info(descr_memory.buffer)

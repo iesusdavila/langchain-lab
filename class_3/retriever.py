d07911305ae501e9e04c62d312c5ea987249ebc9
Iesus Davila <iesusdavila@gmail.com>
2025-07-07 11:04:35 -0500

    Usar un modelo mas preciso para RAG y QA

--- class_3/retriever.py
+++ class_3/retriever.py
@@ -48,7 +48,7 @@ print(retireved_results)
 
 print("=="*20)
 model_llm = LlamaCpp(
-    model_path="models/models--ggml-org--Meta-Llama-3.1-8B-Instruct-Q4_0-GGUF/snapshots/0aba27dd2f1c7f4941a94a5c59d80e0a256f9ff8/meta-llama-3.1-8b-instruct-q4_0.gguf",        
+    model_path="models/Mistral-7B-Instruct-v0.3.IQ4_XS.gguf",
     n_ctx=2048,
     verbose=True,
     n_gpu_layers=20,  

add6854fda92b9ea4c8a8d73ed4575724ec2d25b
Iesus Davila <iesusdavila@gmail.com>
2025-07-09 11:36:14 -0500

    Diferentes formas de Chat Memory - Todas estan deprecadas, pero sirve como base de la teoria y finalidad

---
+++ class_5/conv_buffer_window_memory.py
@@ -0,0 +1,53 @@
+from langchain.memory import ConversationBufferWindowMemory
+from langchain_community.llms.llamacpp import LlamaCpp
+from langchain.chains import ConversationChain
+
+llm = LlamaCpp(
+    # model_path="models/Mistral-7B-Instruct-v0.3.IQ4_XS.gguf",
+    model_path="models/models--MaziyarPanahi--Llama-3.2-1B-Instruct-GGUF/snapshots/b64ae94264258a3d7516a34a8c54928d32b19869/Llama-3.2-1B-Instruct.Q4_K_M.gguf",
+    n_ctx=2048,
+    temperature=0.0,
+    verbose=True,
+    n_gpu_layers=10,  
+    n_threads=8
+)
+
+memory = ConversationBufferWindowMemory(k=20, return_messages=True)
+# memory = ConversationBufferWindowMemory(k=4, return_messages=True)
+
+memory.save_context(
+    {"input": "Hola, mi nombre es Iesus"},
+    {"output": "Hola Iesus, ¿qué tal? Soy un modelo de IA llamado Coco."}
+)
+# Tambien podemos usar memory.chat_memory.add_user_message y memory.chat_memory.add_ai_message
+#memory.chat_memory.add_user_message("Hola, mi nombre es Iesus")
+#memory.chat_memory.add_ai_message("Hola Iesus, ¿qué tal? Soy un modelo de IA llamado Coco.")
+memory.save_context(
+    {"input": "Estoy investigando los diferentes tipos de memoria conversacional."},
+    {"output": "Eso es interesante, ¿cuáles son algunos ejemplos?"}
+)
+memory.save_context(
+    {"input": "He estado viendo ConversationBufferMemory y ConversationBufferWindowMemory."},
+    {"output": "Eso es interesante, ¿cuál es la diferencia?"}
+)
+memory.save_context(
+    {"input": "La memoria buffer solo almacena toda la conversación, ¿verdad?"},
+    {"output": "Eso tiene sentido, ¿y qué hay de ConversationBufferWindowMemory?"}
+)
+memory.save_context(
+    {"input": "La memoria buffer window almacena los últimos k mensajes, descartando el resto."},
+    {"output": "¡Muy interesante!"}
+)
+
+memory.load_memory_variables({})
+
+chain = ConversationChain(
+    llm=llm,
+    memory=memory,
+    verbose=True
+)
+
+response = chain.invoke({"input": "Mi nombre es Iesus o Lionel?."})
+
+print("="*20)
+print(response['response'])
